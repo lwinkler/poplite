a7c9a02921b4d8a01192ad4a6321f4e1575ce8f7
Laurent Winkler <laurent.winkler@bluewin.ch>
2015-05-14 16:35:10 +0200

    Support methods with no arguments

--- parser/parse_broker.hpp
+++ parser/parse_broker.hpp
@@ -2,8 +2,8 @@
 #define _POP_PARSE_BROKER_HPP
 
 // Methods for interface
-#define POP_METH(_calli_, _callb_, _ret_, _meth_, ...) POP_METH_(_calli_, _callb_, _ret_, _meth_, APPLY(ADDNAME, __VA_ARGS__))
-#define POP_METH_(_calli_, _callb_, _ret_, _meth_, ...) inline _ret_ _meth_(APPLY(PAIR, __VA_ARGS__))    {call_sync<_ret_,APPLY(STRIP1, __VA_ARGS__)>(__LINE__,APPLY(STRIP2, __VA_ARGS__));}
+#define POP_METH(_calli_, _callb_, _ret_, _meth_, ...) POP_METH_(_calli_, _callb_, _ret_, _meth_, APPLY(ADDNAME, ## __VA_ARGS__))
+#define POP_METH_(_calli_, _callb_, _ret_, _meth_, ...) inline _ret_ _meth_(APPLY(PAIR, __VA_ARGS__))    {call_sync<_ret_,APPLY(STRIP1, ## __VA_ARGS__)>(__LINE__,APPLY(STRIP2, ## __VA_ARGS__));}
 
 // Constructor 
 #define POP_CONSTR(_calli_, _callb_, _alloc_, ...) POP_CONSTR_(_calli_, _callb_, _alloc_, APPLY(ADDNAME, __VA_ARGS__))
