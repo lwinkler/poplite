2836ad0041bff5d67118a13965187555a63325d0
Laurent Winkler <laurent.winkler@bluewin.ch>
2018-08-22 23:05:10 +0200

    Ping pong can create objects with or without ssh

--- examples/ping_pong/main.cpp
+++ examples/ping_pong/main.cpp
@@ -17,7 +17,8 @@
 using namespace std;
 
 /// Two remote objects are calling each other until counter reaches zero
-
+// TODO: See if we can construct the broker with an object and destroy the thread in destructor
+// TODO: move to its own file
 namespace pop {
 template<typename T> class local_object final {
 	public:
@@ -46,44 +47,62 @@ template<typename T> class local_object final {
 };
 } // namespace pop
 
+
+// Call our interface
+void play_ping_pong(ping_pong_iface& _p, const string& _invoker, unsigned int _nb_calls) {
+	if(_invoker == "async") {
+		pop::mutex_iface l;
+		// objects ping each other asynchrounously
+		_p.async_ping(_nb_calls, l.contact());
+		// note: we need to add a wait here to allow all methods to complete
+		l.wait();
+	} else if(_invoker == "async2") {
+		pop::local_object<pop::mutex> l;
+		pop::accesspoint contact = l.contact();
+		std::thread th([&l] {
+			l.run();
+		});
+
+		// objects ping each other asynchrounously
+		_p.async_ping(_nb_calls, contact);
+
+		// note: we need to add a wait here to allow all methods to complete
+		l.object().wait();
+		l.stop();
+		th.join();
+	} else if(_invoker == "sync") {
+		// objects ping each other synchrounously
+		_p.sync_ping(_nb_calls);
+	} else {
+		cerr << "Unknown invoker " << _invoker << endl;
+	}
+}
+
 int main(int argc, char* argv[]) {
 	// Init the pop system with arguments
 	pop::system::instance(&argc, argv);
 
-	if(argc < 4) {
-		cerr << "usage: " << argv[0] << " <sync or async> <nb pings> <second host>" << endl;
-		return 1;
-	}
 
 	try {
-		// create 2 objects
-		ping_pong_iface p1;
-		ping_pong_iface p2(argv[3]);
-		ping_pong_iface p3;
-		p1.set_next_one(p2.contact());
-		p2.set_next_one(p3.contact());
-		p3.set_next_one(p1.contact()); // TODO remove
+		if(argc == 3) {
+			// create 2 objects
+			ping_pong_iface p1;
+			ping_pong_iface p2;
+			p1.set_next_one(p2.contact());
+			p2.set_next_one(p1.contact());
 
-		if(string(argv[1]) == "async") {
-			// pop::mutex_iface l;
-			pop::local_object<pop::mutex> l;
-			pop::accesspoint contact = l.contact();
-			std::thread th([&l] {
-					l.run();
-			});
+			play_ping_pong(p1, argv[1], boost::lexical_cast<unsigned int>(argv[2]));
+		} else if(argc == 4) {
+			// create 2 objects
+			ping_pong_iface p1;
+			ping_pong_iface p2(argv[3]);
+			p1.set_next_one(p2.contact());
+			p2.set_next_one(p1.contact());
 
-			// objects ping each other asynchrounously
-			p2.async_ping(atoi(argv[2]), contact);
-
-			// note: we need to add a sleep here to allow all methods to complete
-			l.object().wait();
-			l.stop();
-			th.join();
-		} else if(string(argv[1]) == "sync") {
-			// objects ping each other synchrounously
-			p2.sync_ping(atoi(argv[2]));
+			play_ping_pong(p1, argv[1], boost::lexical_cast<unsigned int>(argv[2]));
 		} else {
-			cerr << "Unknown invoker " << argv[1] << endl;
+			cerr << "usage: " << argv[0] << " <sync or async> <nb pings> <second host>" << endl;
+			return 1;
 		}
 
 	} catch (std::exception& e) {
